ca966b1a5d97b6d0598d2be5ea06611530634b23
TenPlus1 <kinsellaja@yahoo.com>
2016-12-28 22:01:26 +0000

    fix nil error

--- cart_entity.lua
+++ cart_entity.lua
@@ -183,8 +183,10 @@ local function rail_on_step(self, dtime)
 
 		if self.count > 10 then
 			minetest.add_item(self.object:getpos(), "carts:cart")
-			minetest.sound_stop(self.sound_handle)
 			self.object:remove()
+			if self.sound_handle then
+				minetest.sound_stop(self.sound_handle)
+			end
 			return
 		end
 
