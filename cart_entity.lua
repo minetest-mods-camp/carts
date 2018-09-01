b67308a72a21f359e72eb893efb4792fbdc1a43c
TenPlus1 <kinsellaja@yahoo.com>
2018-09-01 18:46:15 +0100

    added smalljokers new changes

--- cart_entity.lua
+++ cart_entity.lua
@@ -54,15 +54,19 @@ function cart_entity:on_activate(staticdata, dtime_s)
 		return
 	end
 	self.railtype = data.railtype
-	if data.old_dir then
-		self.old_dir = data.old_dir
+	self.old_dir = data.old_dir or self.old_dir
+	self.old_pos = data.old_pos or self.old_pos
+	-- Correct the position when the cart drives further after the last 'step()'
+	if self.old_pos and boost_cart:is_rail(self.old_pos, self.railtype) then
+		self.object:set_pos(self.old_pos)
 	end
 end
 
 function cart_entity:get_staticdata()
 	return minetest.serialize({
 		railtype = self.railtype,
-		old_dir = self.old_dir
+		old_dir = self.old_dir,
+		old_pos = self.old_pos
 	})
 end
 
